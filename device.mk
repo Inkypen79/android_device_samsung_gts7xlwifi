6bb73208534726da9c27a506ab6e6c83c8e72161
Bruno Martins <bgcngm@gmail.com>
2025-04-30 00:00:08 +0100

    gts7xlwifi: Set UDFPS extension lib via soong config
    Change-Id: I8a709bde4843b61420ea1003a749952d2619e2da

--- device.mk
+++ device.mk
@@ -54,6 +54,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # UDFPS
 $(call soong_config_set,samsung_udfps,udfps_zorder,0x20000000u)
+$(call soong_config_set,surfaceflinger,udfps_lib,//hardware/samsung/fingerprint:libudfps_extension.samsung)
 
 # Inherit from vendor blobs
 $(call inherit-product-if-exists, vendor/samsung/gts7xlwifi/gts7xlwifi-vendor.mk)
