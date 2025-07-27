411d54d95c74ddad1a39f83c7e5eab5070512e36
Atakan <ata.kaner03@gmail.com>
2025-07-27 16:37:57 +0300

    gts7xlwifi: Use Tiramisu build description
    Change-Id: I061e48f98d253935f3f5b12a4a499b99aeec529f

--- lineage_gts7xlwifi.mk
+++ lineage_gts7xlwifi.mk
@@ -25,7 +25,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
 # Use the latest approved GMS identifiers
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="gts7xlwifixx-user 11 RP1A.200720.012 T970XXS7DXH1 release-keys" \
+    BuildDesc="gts7xlwifixx-user 13 TP1A.220624.014 T970XXS7DXH1 release-keys" \
     BuildFingerprint=samsung/gts7xlwifixx/gts7xlwifi:11/RP1A.200720.012/T970XXS7DXH1:user/release-keys \
     DeviceProduct=gts7xlwifixx \
     SystemName=gts7xlwifixx
