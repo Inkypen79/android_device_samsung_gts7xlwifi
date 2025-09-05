d4abc07a7faf194524310b410026cd9fc5604d69
Bruno Martins <bgcngm@gmail.com>
2025-09-05 20:20:28 +0100

    gts7xlwifi: Move UDFPS config to new soong namespace
    Change-Id: Id5eb8f0f0b4962655c593a6ae5c87c4226174e3c

--- device.mk
+++ device.mk
@@ -49,7 +49,7 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
 # UDFPS
-$(call soong_config_set,samsung_udfps,udfps_zorder,0x20000000u)
+$(call soong_config_set,samsungUdfpsVars,udfps_zorder,0x20000000u)
 $(call soong_config_set,surfaceflinger,udfps_lib,//hardware/samsung/fingerprint:libudfps_extension.samsung)
 
 # Inherit from vendor blobs
